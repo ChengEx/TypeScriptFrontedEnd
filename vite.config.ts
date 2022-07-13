b6694d3a4ce60e2c0d3332ef8cc4b02e1e4985dc
ChengEx <frf2isgood@gmail.com>
2022-07-13 13:38:07 +0800

    test1

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,28 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolveComponent } from 'vue'
 
+import { resolve } from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  // resolve: {
+  //   alias: {
+  //     "@": resolve(__dirname, "src"),
+  //   }
+  // },
+  plugins: [vue()],
+  server: {
+    host: '0.0.0.0',
+    port: 8991,
+    https: false,
+    proxy: {
+      '/api': {
+        target: 'http://xxxx:4000',
+        secure: false,
+        changeOrigin: true,
+        
+      }
+    }
+  },
+  
 })
